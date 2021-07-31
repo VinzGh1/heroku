3974da049a64748aaa1bc7868770c80e70893341
VinzGh1 <88178376+VinzGh1@users.noreply.github.com>
2021-08-01 03:04:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir ./dev
 cd ./dev
 mkdir ./tmp
 cd ./tmp
-git clone "https://github.com/libDrive/server.git" --depth 1 server
+git clone "https://github.com/VinzGh1/server.git" --depth 1 server
 git clone "https://github.com/VinzGh1/web.git" --depth 1 web
 mkdir ./libDrive.Server
 mkdir ./libDrive.Server/build
